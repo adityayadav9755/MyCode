075a3b340b93a89bdcf06cbafc11e85c36c250a1
Aditya Yadav <134469067+adityayadav9755@users.noreply.github.com>
2023-08-14 21:12:56 +0530

    Update BinaryDecimal.py

--- BinaryDecimal.py
+++ BinaryDecimal.py
@@ -1,18 +1,11 @@
 def bd(a):
-	b=0
-	n=0
-	out=0
-	while a>0:
-		# Finding the digit
-		b=a%10
-		#Check if digit is only 0 or 1
-		if b==0 or b==1:
-			#Formula for binary to decimal
-			out+=b*(2**n)
-			#Increasing value of n 
-			n+=1
-			# 1//10=0
-			if a!=1:
+	b,n,out=0,0,0
+	while a>0:		
+		b=a%10                       # Finding the digit		
+		if b==0 or b==1:             #Check if digit is only 0 or 1			
+			out+=b*(2**n)        #Formula for binary to decimal			 
+			n+=1                 #Increasing value of n			
+			if a!=1:             # 1//10=0
 				a=a//10
 			else:
 				break
@@ -26,19 +19,15 @@ def db(a):
 	n=0
 	out=0
 	while a>0:
-		#Formula for decimal to binary
-		b=a%2
+		b=a%2                        #Formula for decimal to binary
 		out+=b*(10**n)
 		n+=1
 		a=a//2
 	return out
 
-
-
 choice=int(input("Choose 1:Binary to Decimal  or   2:Decimal to Binary."))
 a=int(input("Enter the number."))
-#Checking the choice.
-if choice==1:
+if choice==1:                                #Checking the choice.
 	print(bd(a))
 elif choice==2:
 	print(db(a))
