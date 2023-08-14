f09df99f917aaed84677821ea936cfea7594e4e5
Aditya Yadav <134469067+adityayadav9755@users.noreply.github.com>
2023-08-14 20:58:19 +0530

    Update and rename Pascal_sTriangle.py to HigherPowersOf11.py

--- HigherPowersOf11.py
+++ HigherPowersOf11.py
@@ -1,10 +1,8 @@
 n=int(input("Enter the maximum number of levels."))
-a=n
 b=1
 while n!=0:
-	print(" "*(n+a), b,  end="\n")
+	print(" "*((n+1)//2), b,  end="\n")
 	b=b*11
 	n-=1
-	a-=1
-TO BE EDITED!!!
+#TO BE EDITED!!!
 
